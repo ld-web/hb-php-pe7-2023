1f0836fc613eca46a5632acff21541b272dadcc0
Lucas Delobelle <lucas@ld-web.net>
2023-09-27 14:01:56 +0200

    :sparkles: Superglobals introduction
    Product item page

---
+++ file-inclusion/products/item.php
@@ -0,0 +1,8 @@
+<?php require_once '../components/head.php'; ?>
+
+<main class="prose prose-lg mx-auto pt-24">
+  <h1>Fiche produit</h1>
+  <!-- CONTENU -->
+</main>
+
+<?php require_once '../components/footer.php';

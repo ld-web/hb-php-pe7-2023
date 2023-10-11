9cbea1b26ca00503d8f7b074802766cfc840b8cf
Lucas Delobelle <lucas@ld-web.net>
2023-10-11 11:59:56 +0200

    :sparkles: OOP intro

---
+++ poo/classes/Product.php
@@ -0,0 +1,88 @@
+<?php
+
+// Définition de la classe Product
+// Structure générale d'un produit
+
+class Product
+{
+    private string $name;
+    private float $priceVatFree;
+    private int $quantity;
+    private bool $discount;
+
+    /**
+     * Gives product's price including given taxes
+     *
+     * @param float $taxRate Between 0 and 1
+     * @return float Full price (including given taxes)
+     */
+    public function getFullPrice(float $taxRate): float
+    {
+        if ($taxRate > 0 && $taxRate < 1) {
+            return $this->priceVatFree * (1 + $taxRate);
+        }
+
+        return $this->priceVatFree;
+    }
+
+    public function getName(): string
+    {
+        return strtoupper($this->name);
+    }
+
+    // Setter
+    public function setPriceVatFree(float $price): void
+    {
+        if ($price >= 0) {
+            $this->priceVatFree = $price;
+        }
+    }
+
+    /**
+     * Get the value of quantity
+     *
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set the value of quantity
+     *
+     * @param int $quantity
+     *
+     * @return self
+     */
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of discount
+     *
+     * @return bool
+     */
+    public function getDiscount(): bool
+    {
+        return $this->discount;
+    }
+
+    /**
+     * Set the value of discount
+     *
+     * @param bool $discount
+     *
+     * @return self
+     */
+    public function setDiscount(bool $discount): self
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+}
---
+++ poo/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// Classe, objet, méthode, instance, extends (héritage), constructeur, encapsulation, attributs (propriétés), visibilité, abstraction, classe abstraite, interface, polymorphisme
+
+// Product : price, description, name, quantity, image, serialNumber...
+
+require_once 'classes/Product.php';
+
+// $product est un objet : une instance de la classe Product
+$product = new Product();
+
+// Impossible d'accéder directement à des propriétés privées
+// $product->discount = true;
+// $product->name = "journey";
+// $product->priceVatFree = 31.68;
+
+// $otherProduct est UNE AUTRE instance de la classe Product
+$otherProduct = new Product();
+// ... Appel d'une fonction de calcul d'un prix...
+// Résultat de cette fonction : -50
+// Problème ?
+$otherProduct->setPriceVatFree(-50);
+var_dump($otherProduct);
+
+// Accéder à la valeur qui se trouve dans la propriété discount de mon instance $product
+// var_dump($product->discount);
+var_dump($product);
+echo $product->getName();

d78b82b7bffc53b3703cfddec7c5ab3fd0242df0
Lucas Delobelle <lucas@ld-web.net>
2023-10-25 15:05:29 +0200

    :sparkles: Sessions / connexion

---
+++ pdo_sessions_auth/classes/AppError.php
@@ -0,0 +1,9 @@
+<?php
+
+class AppError
+{
+    public const DB_CONNECTION = 1;
+    public const AUTH_REQUIRED_FIELDS = 2;
+    public const INVALID_CREDENTIALS   = 3;
+    public const USER_NOT_FOUND = 4;
+}
---
+++ pdo_sessions_auth/profile.php
@@ -0,0 +1,7 @@
+<?php require_once 'layout/header.php'; ?>
+
+<main class="prose mx-auto">
+    <h1>Profil</h1>
+</main>
+
+<?php require_once 'layout/footer.php';
